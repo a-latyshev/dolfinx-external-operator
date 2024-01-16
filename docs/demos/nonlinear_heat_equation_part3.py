cf023329a2df9597622ad146c1f52ecec348b837
Andrey Latyshev <latyshev.andrey.a@gmail.com>
2024-01-16 15:07:29 +0100

    Reflowed

--- docs/demos/nonlinear_heat_equation_part3.py
+++ docs/demos/nonlinear_heat_equation_part3.py
@@ -1,9 +1,15 @@
 # %% [markdown]
 # # Application of external package
 #
-# Here we demonstrate the capabilities of our framework to define the behaviour of the external operator via a 3-rd party Python package. In this example, we use the JAX library to define the external operator $\boldsymbol{q}$ and its derivatives. JAX provides powerful automatic differentiation (AD) and function vectorization features as well as it is able to compile functions just-in-time.
+# Here we demonstrate the capabilities of our framework to define the behaviour of
+# the external operator via a 3-rd party Python package. In this example, we use
+# the JAX library to define the external operator $\boldsymbol{q}$ and its
+# derivatives. JAX provides powerful automatic differentiation (AD) and function
+# vectorization features as well as it is able to compile functions just-in-time.
 #
-# The code solving the heat equation problem from the previous notebook remains almost unchanged. The only functions that must be redefined are `q_impl`, `dqdT_impl` and `dqdsigma_impl`.
+# The code solving the heat equation problem from the previous notebook remains
+# almost unchanged. The only functions that must be redefined are `q_impl`,
+# `dqdT_impl` and `dqdsigma_impl`.
 
 # %%
 import jax.numpy as jnp
@@ -11,17 +17,23 @@ import jax
 jax.config.update("jax_enable_x64", True)
 
 # %% [markdown]
-# By using the vectorization technique we need just to define the behaviour of the function locally at one node of an element. Then we need to reshape the global representation of variables $T$ and $\boldsymbol{\sigma}$ and send them to the vectorized version of the operator $\boldsymbol{q}$ (the function `q_global`). We use the JAX's function `vmap` for the vectorization.
+# By using the vectorization technique we need just to define the behaviour of the
+# function locally at one node of an element. Then we need to reshape the global
+# representation of variables $T$ and $\boldsymbol{\sigma}$ and send them to the
+# vectorized version of the operator $\boldsymbol{q}$ (the function `q_global`).
+# We use the JAX's function `vmap` for the vectorization.
 
 # %%
 
 
 def k(T):
+    # In contrast to the previous implementations, the input `T` here is a scalar.
     return 1.0 / (A + B * T)
 
 
 @jax.jit
 def q(T, sigma):
+    # The input `T is a scalar and sigma is an array with the shape (2,).
     return -k(T) * sigma
 
 
@@ -31,15 +43,22 @@ q_global = jax.jit(jax.vmap(q, in_axes=(0, 0)))
 
 @jax.jit
 def q_impl(T, sigma):
+    # Here we evaluate q globally, so inputs `T` and `sigma` are `np.ndarray` with sizes
+    # cells number * number of interpolation points per cell * local size (which is equal to 1 for T and 2 for sigma).
+    # By applying `reshape((-1, 1))` function we prepare the data for batching.
+    # For example, `sigma_vectorized` has the shape of (cells number * number of interpolation points per cell, 2)
     T_vectorized = T.reshape((-1, 1))
     sigma_vectorized = sigma.reshape((-1, 2))
     out = q_global(T_vectorized, sigma_vectorized)
     return out.reshape(-1)
 
 # %% [markdown]
-# Thus we do not need to think about how to manipulate data globally. Instead, we just define the local behaviour of the operator.
+# Thus we do not need to think about how to manipulate data globally. Instead, we
+# just define the local behaviour of the operator.
 #
-# Once the operator $\boldsymbol{q}$ is defined through a callable Python function we may take the derivative of it using the AD tool via the JAX's function `jacrev`.
+# Once the operator $\boldsymbol{q}$ is defined through a callable Python function
+# we may take the derivative of it using the AD tool via the JAX's function
+# `jacrev`.
 
 
 # %%
@@ -47,7 +66,9 @@ dqdT = jax.jit(jax.jacrev(q, argnums=(0)))
 dqdsigma = jax.jit(jax.jacrev(q, argnums=(1)))
 
 # %% [markdown]
-# As the function `q` acts locally so do the functions `dqdT` and `dqdsigma` because they have the same signature. We can vectorize them and define the global behaviour of the external operator.
+# As the function `q` acts locally so do the functions `dqdT` and `dqdsigma`
+# because they have the same signature. We can vectorize them and define the
+# global behaviour of the external operator.
 
 # %%
 dqdT_global = jax.jit(jax.vmap(dqdT, in_axes=(0, 0)))
