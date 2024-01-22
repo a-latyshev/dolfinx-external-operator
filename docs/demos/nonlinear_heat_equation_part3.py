dc707e48c3118794cbb9e83bf24e7e09b71400f6
Andrey Latyshev <latyshev.andrey.a@gmail.com>
2024-01-22 13:58:33 +0100

    Some improvements of JAX's example

--- docs/demos/nonlinear_heat_equation_part3.py
+++ docs/demos/nonlinear_heat_equation_part3.py
@@ -21,7 +21,9 @@
 # the external operator via a 3-rd party Python package. In this example, we use
 # the JAX library to define the external operator $\boldsymbol{q}$ and its
 # derivatives. JAX provides powerful automatic differentiation (AD) and function
-# vectorization features as well as it is able to compile functions just-in-time.
+# vectorization features as well as it is able to compile functions
+# just-in-time. For more details, visit the
+# [JAX's documentation](https://jax.readthedocs.io/en/latest/index.html).
 #
 # The code solving the heat equation problem from the previous notebook remains
 # almost unchanged. The only functions that must be redefined are `q_impl`,
@@ -30,14 +32,16 @@
 # %%
 import jax
 
+# enables the work with double precision under float64
 jax.config.update("jax_enable_x64", True)
 
 # %% [markdown]
-# By using the vectorization technique we need just to define the behaviour of the
-# function locally at one node of an element. Then we need to reshape the global
+# We use JAX's auto-vectorisation decorator `jax.vmap` to allow us to write the
+# function definition for a single interpolation point, and automatically extend
+# that definition to a batch of interpolation points. Then we need to reshape the global
 # representation of variables $T$ and $\boldsymbol{\sigma}$ and send them to the
 # vectorized version of the operator $\boldsymbol{q}$ (the function `q_global`).
-# We use the JAX's function `vmap` for the vectorization.
+# automatically extend that definition to a batch of interpolation points.
 
 # %%
 A = 1.0
@@ -75,7 +79,7 @@ def q_impl(T, sigma):
 #
 # Once the operator $\boldsymbol{q}$ is defined through a callable Python function
 # we may take the derivative of it using the AD tool via the JAX's function
-# `jacfwd`.
+# `jax.jacfwd`.
 
 
 # %%
