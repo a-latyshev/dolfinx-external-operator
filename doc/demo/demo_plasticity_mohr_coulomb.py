9f51c5472479bf1a81467cf0a90f93caad76ab6e
Jack S. Hale <mail@jackhale.co.uk>
2024-10-11 11:13:57 +0200

    Ruff.

--- doc/demo/demo_plasticity_mohr_coulomb.py
+++ doc/demo/demo_plasticity_mohr_coulomb.py
@@ -775,9 +775,11 @@ if len(points_on_process) > 0:
 
 # %%
 try:
-    import pyvista  # noqa: I001
+    import pyvista
+
     print(pyvista.global_theme.jupyter_backend)
     import dolfinx.plot
+
     pyvista.start_xvfb(0.1)
 
     W = fem.functionspace(domain, ("Lagrange", 1, (gdim,)))
