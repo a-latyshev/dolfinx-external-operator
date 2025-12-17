863ab0bc65cd24fc50699fcee6c9d978ffb4d1d5
Andrey Latyshev <latyshev.andrey.a@gmail.com>
2025-12-17 23:16:01 +0000

    Better test for DG

--- test/test_operator_evaluation.py
+++ test/test_operator_evaluation.py
@@ -1,6 +1,5 @@
 # Author: Andrey Latyshev
 # Test external evaluation of external operators
-# Test is based on the heat equation tutorial: `nonlinear_heat_equation_part2.py`.
 
 from mpi4py import MPI
 
@@ -8,7 +7,9 @@ import numpy as np
 
 import basix
 import ufl
-import ufl.algorithms
+from ufl.algorithms import expand_derivatives
+from ufl import Measure, TestFunction, TrialFunction, derivative, grad, inner, split, div
+
 from dolfinx import fem, mesh
 from dolfinx_external_operator import (
     FEMExternalOperator,
@@ -16,10 +17,38 @@ from dolfinx_external_operator import (
     evaluate_operands,
     replace_external_operators,
 )
-from ufl import Measure, TestFunction, TrialFunction, derivative, grad, inner
 
+def check_vector_matrix(F, F_explicit, u):
+    """"Check that the vector and matrix assembled from the form `F`
+    match those assembled from the explicit form F_explicit. The derivative is
+    taken in the direction of the function `u`.
+    """
+    V = u.function_space
+    J = derivative(F, u, TrialFunction(V))
+    J_expanded = expand_derivatives(J)
+    F_replaced, F_external_operators = replace_external_operators(F)
+    J_replaced, J_external_operators = replace_external_operators(J_expanded)
+    evaluated_operands = evaluate_operands(F_external_operators)
+    _ = evaluate_external_operators(F_external_operators, evaluated_operands)
+    _ = evaluate_external_operators(J_external_operators, evaluated_operands)
+
+    F_compiled = fem.form(F_replaced)
+    J_compiled = fem.form(J_replaced)
+    b_vector = fem.assemble_vector(F_compiled)
+    A_matrix = fem.assemble_matrix(J_compiled)
+
+    F_explicit_compiled = fem.form(F_explicit)
+    b_explicit_vector = fem.assemble_vector(F_explicit_compiled)
+    assert np.allclose(b_explicit_vector.array, b_vector.array)
+
+    J_explicit = derivative(F_explicit, u, TrialFunction(V))
+    J_explicit_compiled = fem.form(J_explicit)
+    A_explicit_matrix = fem.assemble_matrix(J_explicit_compiled)
+    assert np.allclose(A_explicit_matrix.to_dense(), A_matrix.to_dense())
 
 def test_quadrature_space():
+    # Test is based on the heat equation tutorial:
+    # `nonlinear_heat_equation_part2.py`.
     domain = mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
     V = fem.functionspace(domain, ("P", 1))
 
@@ -33,7 +62,6 @@ def test_quadrature_space():
     Q = fem.functionspace(domain, Qe)
     W = fem.functionspace(domain, ("P", 1, (2,)))
     dx = Measure("dx", metadata={"quadrature_degree": quadrature_degree})
-
 
     A = 1.0
     B = 1.0
@@ -73,98 +101,52 @@ def test_quadrature_space():
     T_tilde = TestFunction(V)
 
     F = inner(q_, grad(T_tilde)) * dx
-
-    T_hat = TrialFunction(V)
-    J = derivative(F, T, T_hat)
-    J_expanded = ufl.algorithms.expand_derivatives(J)
-    F_replaced, F_external_operators = replace_external_operators(F)
-    J_replaced, J_external_operators = replace_external_operators(J_expanded)
-    evaluated_operands = evaluate_operands(F_external_operators)
-    _ = evaluate_external_operators(F_external_operators, evaluated_operands)
-    _ = evaluate_external_operators(J_external_operators, evaluated_operands)
-
-    F_compiled = fem.form(F_replaced)
-    J_compiled = fem.form(J_replaced)
-    b_vector = fem.assemble_vector(F_compiled)
-    A_matrix = fem.assemble_matrix(J_compiled)
-
     k_explicit = 1.0 / (A + B * T)
     q_explicit = -k_explicit * sigma
     F_explicit = inner(q_explicit, grad(T_tilde)) * dx
-    F_explicit_compiled = fem.form(F_explicit)
-    b_explicit_vector = fem.assemble_vector(F_explicit_compiled)
 
-    assert np.allclose(b_explicit_vector.array, b_vector.array)
-
-    J_explicit = ufl.derivative(F_explicit, T, T_hat)
-    J_explicit_compiled = fem.form(J_explicit)
-    A_explicit_matrix = fem.assemble_matrix(J_explicit_compiled)
-    assert np.allclose(A_explicit_matrix.to_dense(), A_matrix.to_dense())
-
-    J_manual = (
-        inner(B * k_explicit**2 * sigma * T_hat, grad(T_tilde)) * dx
-        + inner(-k_explicit * ufl.Identity(2) * grad(T_hat), grad(T_tilde)) * dx
-    )
-    J_manual_compiled = fem.form(J_manual)
-    A_manual_matrix = fem.assemble_matrix(J_manual_compiled)
-    assert np.allclose(A_manual_matrix.to_dense(), A_matrix.to_dense())
-
+    check_vector_matrix(F, F_explicit, T)
 
 def test_discontinuous_space():
     domain = mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
     gdim = domain.geometry.dim
     V = fem.functionspace(domain, ("P", 1, (gdim,)))
     u = fem.Function(V)
+    u.interpolate(lambda x: (x[0], x[1]))
 
-    dx = Measure("dx", metadata={"quadrature_degree": 1})
+    def N_impl(div_u_, grad_u_):
+        # each operand has shape: (n_cells, n_dofs_per_cell, *math_shape)
+        # N = div(u) + grad(u):grad(u)
+        # shape(grad(u):grad(u)) == (n_cells, n_dofs_per_cell)
+        return div_u_.reshape(-1) + np.einsum("...ij,...ij->...", grad_u_, grad_u_).reshape(-1)
 
-    def N_impl(u_, grad_u_):
-        return np.dot(u_.reshape(-1), u_.reshape(-1)) + np.dot(grad_u_.reshape(-1), grad_u_.reshape(-1))
+    def dNddivu_impl(div_u_, grad_u_):
+        n_cells = div_u_.shape[0]
+        n_dofs = div_u_.shape[1]
+        return np.ones(n_cells*n_dofs).reshape(-1)
 
-    def dNdu_impl(u_, grad_u_):
-        return 2.0 * u_.reshape(-1)
-
-    def dNdgradu_impl(u_, grad_u_):
-        return 2.0 * grad_u_.reshape(-1)
+    def dNdgradu_impl(div_u_, grad_u_):
+        return 2 * grad_u_.reshape(-1)
 
     def N_external(derivatives):
-        if derivatives == (0, 0):
+        if derivatives == (0,0):
             return N_impl
-        elif derivatives == (1, 0):
-            return dNdu_impl
-        elif derivatives == (0, 1):
+        elif derivatives == (1,0):
+            return dNddivu_impl
+        elif derivatives == (0,1):
             return dNdgradu_impl
         else:
             raise NotImplementedError
-    Q = fem.functionspace(domain, ("DG", 0, ()))
-    N = FEMExternalOperator(u, grad(u), function_space=Q, external_function=N_external)
+
+    Q = fem.functionspace(domain, ("DG", 1, ()))
+    N = FEMExternalOperator(div(u), grad(u), function_space=Q, external_function=N_external)
     v = TestFunction(V)
 
+    dx = ufl.Measure("dx", metadata={"quadrature_degree": 2})
     F = N * inner(u, v) * dx
-
-    J = derivative(F, u, TrialFunction(V))
-    J_expanded = ufl.algorithms.expand_derivatives(J)
-    F_replaced, F_external_operators = replace_external_operators(F)
-    J_replaced, J_external_operators = replace_external_operators(J_expanded)
-    evaluated_operands = evaluate_operands(F_external_operators)
-    _ = evaluate_external_operators(F_external_operators, evaluated_operands)
-    _ = evaluate_external_operators(J_external_operators, evaluated_operands)
-
-    F_compiled = fem.form(F_replaced)
-    J_compiled = fem.form(J_replaced)
-    b_vector = fem.assemble_vector(F_compiled)
-    A_matrix = fem.assemble_matrix(J_compiled)
-
-    N_explicit = inner(u, u) + inner(grad(u), grad(u))
+    N_explicit = div(u) + inner(grad(u), grad(u))
     F_explicit = N_explicit * inner(u, v) * dx
-    F_explicit_compiled = fem.form(F_explicit)
-    b_explicit_vector = fem.assemble_vector(F_explicit_compiled)
-    assert np.allclose(b_explicit_vector.array, b_vector.array)
-
-    J_explicit = ufl.derivative(F_explicit, u, TrialFunction(V))
-    J_explicit_compiled = fem.form(J_explicit)
-    A_explicit_matrix = fem.assemble_matrix(J_explicit_compiled)
-    assert np.allclose(A_explicit_matrix.to_dense(), A_matrix.to_dense())
+    check_vector_matrix(F, F_explicit, u)
 
 def test_continuous_space():
     domain = mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
@@ -193,40 +175,17 @@ def test_continuous_space():
     N = FEMExternalOperator(u, function_space=V, external_function=N_external)
     v = TestFunction(V)
     F = inner(N, v) * ufl.dx
-
-    J = derivative(F, u, TrialFunction(V))
-    J_expanded = ufl.algorithms.expand_derivatives(J)
-    F_replaced, F_external_operators = replace_external_operators(F)
-    J_replaced, J_external_operators = replace_external_operators(J_expanded)
     
-    evaluated_operands = evaluate_operands(F_external_operators)
-    _ = evaluate_external_operators(F_external_operators, evaluated_operands)
-    _ = evaluate_external_operators(J_external_operators, evaluated_operands)
-
-    F_compiled = fem.form(F_replaced)
-    J_compiled = fem.form(J_replaced)
-    b_vector = fem.assemble_vector(F_compiled)
-    A_matrix = fem.assemble_matrix(J_compiled)
-
-    # Check vector assembly
     N_explicit = u
     F_explicit = inner(N_explicit, v) * ufl.dx
-    F_explicit_compiled = fem.form(F_explicit)
-    b_explicit_vector = fem.assemble_vector(F_explicit_compiled)
-    assert np.allclose(b_explicit_vector.array, b_vector.array)
-
-    # Check matrix assembly
-    J_explicit = ufl.derivative(F_explicit, u, TrialFunction(V))
-    J_explicit_compiled = fem.form(J_explicit)
-    A_explicit_matrix = fem.assemble_matrix(J_explicit_compiled)
-    assert np.allclose(A_explicit_matrix.to_dense(), A_matrix.to_dense())
+    check_vector_matrix(F, F_explicit, u)
 
 def test_mixed_element_space():
     domain = mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
     gdim = domain.geometry.dim
     # Measure
     quadrature_degree = 2
-    dx = ufl.Measure(
+    dx = Measure(
         "dx",
         domain=domain,
         metadata={"quadrature_scheme": "default", "quadrature_degree": quadrature_degree},
@@ -238,8 +197,8 @@ def test_mixed_element_space():
     u = fem.Function(V)
     u.sub(0).interpolate(lambda x: x[1] + 2.0)
     u.sub(1).interpolate(lambda x: x[1] + 1.0)
-    u1, u2 = ufl.split(u)
-    v = ufl.TestFunction(V)
+    u1, u2 = split(u)
+    v = TestFunction(V)
 
     V1 = V.sub(0)
     V2 = V.sub(1)
@@ -267,31 +226,9 @@ def test_mixed_element_space():
     
     # u2 from V2 will be projected onto both V1 and V2
     N = FEMExternalOperator(u2, function_space=V, name="N", external_function=N_external)
-    N1, N2 = ufl.split(N)
-    v1, v2 = ufl.split(v)
-    F = N1 * v1 * ufl.dx + ufl.inner(ufl.grad(N2), v) * ufl.dx
-    J = ufl.derivative(F, u, ufl.TrialFunction(V))
-    J_expanded = ufl.algorithms.expand_derivatives(J)
+    N1, N2 = split(N)
+    v1, v2 = split(v)
+    F = N1 * v1 * ufl.dx + inner(grad(N2), v) * ufl.dx
+    F_explicit = inner(grad(u2), v) * ufl.dx
 
-    F_replaced, F_external_operators = replace_external_operators(F)
-    J_replaced, J_external_operators = replace_external_operators(J_expanded)
-    evaluated_operands = evaluate_operands(F_external_operators)
-    _ = evaluate_external_operators(F_external_operators, evaluated_operands)
-    _ = evaluate_external_operators(J_external_operators, evaluated_operands)
-
-    F_compiled = fem.form(F_replaced)
-    J_compiled = fem.form(J_replaced)
-    b_vector = fem.assemble_vector(F_compiled)
-    A_matrix = fem.assemble_matrix(J_compiled)
-
-    # Check vector assembly
-    F_explicit = inner(ufl.grad(u2), v) * ufl.dx
-    F_explicit_compiled = fem.form(F_explicit)
-    b_explicit_vector = fem.assemble_vector(F_explicit_compiled)
-    assert np.allclose(b_explicit_vector.array, b_vector.array)
-
-    # Check matrix assembly
-    J_explicit = ufl.derivative(F_explicit, u, TrialFunction(V))
-    J_explicit_compiled = fem.form(J_explicit)
-    A_explicit_matrix = fem.assemble_matrix(J_explicit_compiled)
-    assert np.allclose(A_explicit_matrix.to_dense(), A_matrix.to_dense())
+    check_vector_matrix(F, F_explicit, u)
