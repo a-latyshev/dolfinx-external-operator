9d2909913bd2ced129c09489a8989e285793e03a
Andrey Latyshev <latyshev.andrey.a@gmail.com>
2026-07-10 10:34:22 +0200

    Optimize operand evaluation by precomputing lookup keys and unifying evaluator access

--- src/dolfinx_external_operator/external_operator.py
+++ src/dolfinx_external_operator/external_operator.py
@@ -237,7 +237,11 @@ class FEMExternalOperator(ufl.ExternalOperator):
         op_ref_fs = self.ref_function_space
         op_mesh = op_ref_fs.mesh
         for operand in self.ufl_operands:
-            if not isinstance(operand, ufl.ExternalOperator):
+            if isinstance(operand, ufl.ExternalOperator):
+                def make_nested_evaluator(op=operand):
+                    return lambda entities: evaluate_operands([op], entities)
+                eval_func = make_nested_evaluator()
+            else:
                 operand_domain = ufl.domain.extract_unique_domain(operand)
                 if operand_domain == op_ref_fs.ufl_domain():
                     operand_mesh = op_mesh
@@ -267,8 +271,13 @@ class FEMExternalOperator(ufl.ExternalOperator):
                     def make_standard_evaluator(e=expr, m=operand_mesh):
                         return lambda entities: e.eval(m, entities)
                     eval_func = make_standard_evaluator()
-                
-                self._compiled_operands[operand] = eval_func
+            
+            self._compiled_operands[operand] = eval_func
+
+        # Precompute the lookup keys for each operand
+        self._operand_keys = {
+            op: (self._eval_points_bytes, op) for op in self.ufl_operands
+        }
 
     def _ufl_expr_reconstruct_(
         self,
@@ -425,14 +434,10 @@ def evaluate_operands(
     evaluated_operands = {}
     for external_operator in external_operators:
         for operand in external_operator.ufl_operands:
-            key = (external_operator._eval_points_bytes, operand)
+            key = external_operator._operand_keys[operand]
             if key not in evaluated_operands:
-                if isinstance(operand, ufl.ExternalOperator):
-                    evaluated_operand = evaluate_operands([operand], entities)
-                else:
-                    eval_func = external_operator._compiled_operands[operand]
-                    evaluated_operand = eval_func(entities)
-                evaluated_operands[key] = evaluated_operand
+                eval_func = external_operator._compiled_operands[operand]
+                evaluated_operands[key] = eval_func(entities)
     return evaluated_operands
 
 
@@ -456,7 +461,7 @@ def evaluate_external_operators(
     for external_operator in external_operators:
         ufl_operands_eval = []
         for operand in external_operator.ufl_operands:
-            key = (external_operator._eval_points_bytes, operand)
+            key = external_operator._operand_keys[operand]
             if isinstance(operand, ufl.ExternalOperator):
                 sub_eval_ops = evaluated_operands[key]
                 ufl_operands_eval.extend(evaluate_external_operators([operand], sub_eval_ops))
