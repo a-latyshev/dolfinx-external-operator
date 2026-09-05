ebbc2645d971036a9aea53b970865aa040d5a63a
Mr-Neutr0n <64578610+Mr-Neutr0n@users.noreply.github.com>
2026-09-05 08:56:31 +0000

    Fix #36: Mixed elements
    Signed-off-by: Mr-Neutr0n <64578610+Mr-Neutr0n@users.noreply.github.com>

--- test/test_external_operators_construction.py
+++ test/test_external_operators_construction.py
@@ -194,6 +194,36 @@ def test_indexed_operands():
     assert renumber_indices(N.ufl_operands[0]) == renumber_indices(dN.ufl_operands[0])
 
 
+def test_mixed_solution_operand_expansion():
+    """Regression test for issue #36: Jacobian operands must match residual operands."""
+    domain = create_unit_square(MPI.COMM_WORLD, 2, 2)
+    gdim = domain.geometry.dim
+
+    Ve = basix.ufl.element("P", domain.topology.cell_name(), 1, shape=(gdim,))
+    Se = basix.ufl.element("P", domain.topology.cell_name(), 1)
+    M = fem.functionspace(domain, basix.ufl.mixed_element([Ve, Se]))
+    Q = Q_gen(domain, (gdim,))
+
+    w = fem.Function(M)
+    u, T = ufl.split(w)
+    grad_u = ufl.grad(u)
+    temp = T
+
+    N = FEMExternalOperator(grad_u, temp, function_space=Q)
+    vu, _ = ufl.split(ufl.TestFunction(M))
+    F = ufl.inner(N, vu) * ufl.dx
+    J = ufl.derivative(F, w, ufl.TrialFunction(M))
+    _, J_external_operators = replace_external_operators(J)
+
+    grad_u_expanded = renumber_indices(apply_algebra_lowering(grad_u))
+    temp_expanded = renumber_indices(apply_algebra_lowering(temp))
+    assert renumber_indices(N.ufl_operands[0]) == grad_u_expanded
+    assert renumber_indices(N.ufl_operands[1]) == temp_expanded
+    for dN in J_external_operators:
+        assert renumber_indices(dN.ufl_operands[0]) == grad_u_expanded
+        assert renumber_indices(dN.ufl_operands[1]) == temp_expanded
+
+
 def test_no_operator():
     domain = create_unit_square(MPI.COMM_WORLD, 2, 2)
     V = fem.functionspace(domain, ("P", 1))
